d8ce6a2799989f4856b8f53cf344bc4afc2407ca
John Noble <j.nble92@gmail.com>
2023-04-19 14:32:35 -0400

    reactive colors and particles

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
 
+const container = document.getElementById('audio-container')
+const file = document.getElementById('fileupload')
+
+let audioSource;
+let analyzer;
+
+
+
+file.addEventListener('change', function () {
+    const files = file.files
+    const audio1 = document.getElementById('audio1');
+    audio1.src = URL.createObjectURL(files[0]);
+    audio1.load();
+    audio1.play();
+
+    const audioCtx = new AudioContext();
+    audioSource = audioCtx.createMediaElementSource(audio1);
+    // Connects to analyzer node
+    analyzer = audioCtx.createAnalyser();
+    audioSource.connect(analyzer);
+    // Connect to speakers
+    analyzer.connect(audioCtx.destination)
+    // Sets FFT size?
+    //TODO: Look in to FFT More.
+    // FFT or Fast Fourier Transforms music in terms of time to terms of frequency where the x-axis is frequency and the y-axis is amplitude.
+    analyzer.fftSize = 2048;
+    const bufferLength = analyzer.frequencyBinCount;
+    //converting bufferLength to what's needed for the array. this is what visualizes the waveform
+    //Can only be Uint8Array because getByteFrequencyData() is designed to return a array of frequency data in the form of 8bit integers
+    const dataArray = new Uint8Array(bufferLength);
+
+    const barWidth = 3
+    let barHeight;
+    let x;
+
+// -----------------------------Particles And Lines--------------------------------------------------------------------------------------------
+
+
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d');
 const pts = document.getElementById('points')
 const strt = document.getElementById('start')
+
 canvas.width = window.innerWidth
+
 canvas.height = window.innerHeight
 let active = true;
 let points =  0 ;
@@ -41,7 +81,14 @@ class Particle {
         this.directionY = directionY;
         this.size = size;
         this.color = color;
+
+         
     }
+
+    resize(minSize, maxSize) {
+        this.size = Math.random() * (maxSize - minSize) + minSize;
+      }
+
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
@@ -50,7 +97,14 @@ class Particle {
         // The particle
     }
 
+    // ChatGPT's idea for maintaining the initial state of the fame
+    reset() {
+        this.directionX = (Math.random() * 2) - 2.5;
+        this.directionY = (Math.random() * 2) - 2.5;
+        // this.directionX = 0
+        // this.directionY = 0
     
+    }
 
     // check particle position, check mouse position, move and draw the particle
     update() {
@@ -73,6 +127,8 @@ class Particle {
         this.y += this.directionY;
 
 
+       
+
         //position of incidence(where in space does it hits)
         if (distance *8 <= this.size + mouse.radius) {
 
@@ -90,13 +146,58 @@ class Particle {
             this.directionY = Math.sin(reflectionAngle);
 
             // Triggers game over functionality
-            gameOver()
-    }
+                canvas.style.background = 'hsl(0, 0%, 100%)';
+                pts.style.top = '50%'
+                pts.style.left = '50%'
+            
+                active = false
+                console.log(points)
+                this.directionX = this.directionX/2
+                this.directionX = this.directionX/2
+            
+                particlesArray = particlesArray.filter((particles) => { 
+                    return particles.directionX == this.directionX
+            
+                })
+                particlesArray.forEach((particle) => { 
+                     particle.color = "#000000"
+            
+                })
+
+                particlesArray.reset;
+            
+            
+
+
+
+                
+                }
+            
 
 
 // draw particle
 this.draw();
     }
+
+}
+
+function restartGame() {
+    active = true;
+    pts.innerText = 0
+    clearInterval(intervalId);
+
+    intervalId = setInterval(incrementPoints, 1000);
+
+    // Reset the BG
+
+                pts.style.top = '90%'
+                pts.style.left = '90%'
+                strt.innerText = ""
+
+    // Reset the points
+    points = 0;
+    // Reset the game state
+    canvas.style.background = 'radial-gradient(#ffc38c, #ff9b40);';
 
 }
 
@@ -142,7 +243,8 @@ function connect() {
                 // GOAL: The lines will be breakable by the mouse
             if (distance < (canvas.width / 7) * (canvas.height / 7)) {
                 opacityValue = 1 - (distance / 20000)
-                ctx.strokeStyle = 'rgba(140,85,31,' + opacityValue + ')';
+                // ctx.strokeStyle = 'rgba(140,85,31,' + opacityValue + ')';
+                ctx.strokeStyle = particlesArray[a].color ;
                 ctx.lineWidth = 2;
 
                 // This creates each line
@@ -188,7 +290,6 @@ midpointy: (particlesArray[a].y + particlesArray[b].y)/2
 }
 
 function init() {
-
     particlesArray = [];
     let numberOfParticles = (canvas.height * canvas.width) / 9000;
     for (let i = 0; i < numberOfParticles; i++) {
@@ -196,18 +297,26 @@ function init() {
         let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
         let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
 
-        let directionX = (Math.random() * 5) - 2.5
-        let directionY = (Math.random() * 5) - 2.5
+        let directionX = 0
+        let directionY = 0
         let color = '#8C5523';
 
-        particlesArray.push(new Particle(x, y, directionX, directionY, size, color));
+        let particle = new Particle(x, y, directionX, directionY, size, color);
+        particlesArray.push(particle);
 
-    
+
+        resetParticles()
+
+
     }
-// Gonna make each particle change in size a bit
 
 }
 
+function resetParticles() {
+    particlesArray.forEach((particle) => {
+        particle.reset();
+    });
+}
 
 
 // animation loop
@@ -215,35 +324,50 @@ function init() {
 function animate() {
 
     requestAnimationFrame(animate);
+    analyzer.getByteFrequencyData(dataArray);
+
     ctx.clearRect(0, 0, innerWidth, innerHeight);
     for (let i = 0; i < particlesArray.length; i++) {
         particlesArray[i].update()
+        
+        const red = i * dataArray[i] / 2;
+        const green = i *dataArray[i] / 2;
+        const blue = i*dataArray[i] / 2;
+        if (active == true){
+            canvas.style.background = 'rgb('+ red/60 +',' + green/60 + ',' + blue/60 + ')';
+            }
+        particlesArray.forEach(particle => {
+            //resize particles using music data
+            particle.resize(particle.size, dataArray[i]/15); 
+            // particle.style.filter = 'invert(100%)'
+
+// parse the background color into an RGB color value
+const backgroundColor = canvas.style.background;
+
+
+const rgbValues = backgroundColor.match(/\d+/g).map(Number);
+
+// calculate the inverse of each RGB color component
+const invertedRgbValues = rgbValues.map(value => 255 - value);
+
+// combine the inverted RGB values into a CSS color string
+const invertedColor = `rgb(${invertedRgbValues.join(', ')})`;
+
+// set the particle color to the inverted color
+particle.color = invertedColor;
+            // particle.color = 'rgb('+ red +',' + green + ',' + blue + ')'
+
+          });
+
+          
+
     }
     connect();
 
 
 }
 
-function gameOver(){
-    canvas.style.background = 'hsl(0, 0%, 100%)';
-    pts.style.top = '50%'
-    pts.style.left = '50%'
 
-    active = false
-    console.log(points)
-    this.directionX = this.directionX/2
-    this.directionX = this.directionX/2
-
-    particlesArray = particlesArray.filter((particles) => { 
-        return particles.directionX == this.directionX
-
-    })
-    particlesArray.forEach((particle) => { 
-         particle.color = "#000000"
-
-    })
-
-}
 
 // resize event
 window.addEventListener('resize', function () {
@@ -261,13 +385,17 @@ window.addEventListener('mouseout',
 
 
 function start(){
+
+    // Reload the current page
+container.style.zIndex = 0
 init();
+if(active == true){
 animate();
-
 }
-
-
-start()
+else{
+    restartGame()
+}
+}
 
 
 // Point Counter
@@ -278,11 +406,17 @@ function incrementPoints() {
   pts.innerText = points
 //   console.log(`Points: ${points}`);
   
-  // Stop incrementing points after 10 seconds
-  if (!active) {
+
+if (!active) {
 
     clearInterval(intervalId);
     pts.innerText = "Gameover"
     strt.innerText = "retry"
+
   }
 }
+
+
+start()
+
+})
